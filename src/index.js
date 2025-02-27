4caa714c49c6137c4e0d666b4293e2331989f314
Alexey_Pankov <pankov.kek@gmail.com>
2025-02-27 22:41:59 +0300

    added clock

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const secSpan = document.getElementById('sec')
 const dateSpan = document.getElementById('date')
 const monthSpan = document.getElementById('month')
 const yearSpan = document.getElementById('year')
+const secHand = document.getElementById('sec_hand')
+const minHand = document.getElementById('min_hand')
+const hrsHand = document.getElementById('hrs_hand')
 
 const getMonthName = (date) => {
   let names = [
@@ -22,8 +25,8 @@ const getMonthName = (date) => {
   return names[date]
 }
 
-const addNullToSeconds = (seconds) => {
-  switch (seconds) {
+const addNullToTime = (time) => {
+  switch (time) {
     case 0: 
       return '00'
     case 1:
@@ -45,7 +48,7 @@ const addNullToSeconds = (seconds) => {
     case 9:
       return '09'
     default:
-      return seconds    
+      return time    
   }
 }
 
@@ -53,11 +56,18 @@ const timerHandler = () => {
   const date = new Date()
 
   hrsSpan.innerHTML = date.getHours()
-  minSpan.innerHTML = date.getMinutes()
-  secSpan.innerHTML = addNullToSeconds(date.getSeconds())
+  minSpan.innerHTML = addNullToTime(date.getMinutes())
+  secSpan.innerHTML = addNullToTime(date.getSeconds())
   dateSpan.innerHTML = date.getDate()
   monthSpan.innerHTML = getMonthName(date.getMonth())
   yearSpan.innerHTML = date.getFullYear()
+
+  h = 30 * (date.getHours() % 12 + date.getMinutes() / 60); 
+  m = 6 * date.getMinutes();
+  s = 6 * date.getSeconds();
+  secHand.style.cssText = `transform: rotate(${s}deg);`
+  minHand.style.cssText = `transform: rotate(${m}deg);`
+  hrsHand.style.cssText = `transform: rotate(${h}deg);`
 }
 
 window.setInterval(timerHandler, 1000)
